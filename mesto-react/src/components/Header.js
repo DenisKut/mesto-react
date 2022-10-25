11519685ee10e69d7184ae9dc5ca7813c9c0bc7e
DenisKut <tarelko_den@list.ru>
2022-10-25 14:32:08 +0300

    10_sprint, First_review!!!

--- mesto-react/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import logo from "../images/Vector.svg";
-
-export default function Header () {
-  return(
-    <header className="header">
-      <img className="header__logo" src={logo} alt="логотип MESTO RUSSIA"/>
-    </header>
-  )
-}
